81776996eb9e7d249fea8363b8de5be5e8045a93
Zachary Durr <zdurr22@gmail.com>
2020-09-24 19:11:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -118,7 +118,7 @@ def play(board)
   end
 
   if won?(board)
-    puts "Congratulations #{winner}"
+    puts "Congratulations #{winner(board)}"
   elsif draw?(board) == true
     puts "Cat's Game!"
   end
