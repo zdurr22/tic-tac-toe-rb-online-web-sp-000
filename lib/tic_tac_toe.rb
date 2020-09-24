844074d1b03e5b2b186379385924f030826b44c7
Zachary Durr <zdurr22@gmail.com>
2020-09-24 18:57:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -98,3 +98,15 @@ def turn_count(board)
   end
   counter
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
