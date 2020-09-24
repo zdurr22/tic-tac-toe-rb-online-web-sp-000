06872de8f2379dcec46d3c2279f157febbec8167
Zachary Durr <zdurr22@gmail.com>
2020-09-24 19:07:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,20 +55,6 @@ def over?(board)
   end
 end
 
-def winner(board)
-  index = []
-  index = won?(board)
-  if index == false
-    return nil
-  else
-    if board[index[0]] == "X"
-      return "X"
-    else
-      return "O"
-    end
-  end
-end
-
 def input_to_index(user_input)
   user_input.to_i - 1
 end
