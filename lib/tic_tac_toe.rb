7d2deca9b30b34be41d8f60c70bb1c3c2440d2f4
Zachary Durr <zdurr22@gmail.com>
2020-09-24 18:58:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -104,7 +104,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, current_player)
     display_board(board)
   else
     turn(board)
