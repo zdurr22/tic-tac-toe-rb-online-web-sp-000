d4be6f67c146b1e995628c3512bfd7459e55274c
Zachary Durr <zdurr22@gmail.com>
2020-09-24 19:08:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -109,17 +109,3 @@ def play(board)
     puts "Cat's Game!"
   end
 end
-
-def winner(board)
-  index = []
-  index = won?(board)
-  if index == false
-    return nil
-  else
-    if board[index[0]] == "X"
-      return "X"
-    else
-      return "O"
-    end
-  end
-end
