1cc8d62fdbfa98c4ba1914c3dd5c61cdb7405068
Zachary Durr <zdurr22@gmail.com>
2020-09-24 18:42:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,11 @@
 
-
+def display_board
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
