dd3b6ec56b15c624f5c3b10d4b0348aeac4ee3c5
Zachary Durr <zdurr22@gmail.com>
2020-09-24 19:08:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -98,6 +98,20 @@ def turn(board)
   end
 end
 
+def winner(board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
+
 def play(board)
   until over?(board) == true
     turn(board)
